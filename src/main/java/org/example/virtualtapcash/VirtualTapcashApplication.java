13a99715c852c8f67cd7aa7be0255059c0b714f0
merwinfattah <merwinfattah@gmail.com>
2024-04-19 15:54:28 +0700

    Initial commit

---
+++ src/main/java/org/example/virtualtapcash/VirtualTapcashApplication.java
@@ -0,0 +1,13 @@
+package org.example.virtualtapcash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualTapcashApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VirtualTapcashApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/virtualtapcash/VirtualTapcashApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.virtualtapcash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualTapcashApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
