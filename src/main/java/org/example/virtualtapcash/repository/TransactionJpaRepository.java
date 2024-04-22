1c9b21c9c7840525f533399c316ccc5b8d8d6752
yellyulfita <yelliiyulfita@gmail.com>
2024-04-22 14:57:09 +0700

    @trans

---
+++ src/main/java/org/example/virtualtapcash/repository/TransactionJpaRepository.java
@@ -0,0 +1,11 @@
+package org.example.virtualtapcash.repository;
+
+
+import org.example.virtualtapcash.models.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransactionJpaRepository extends JpaRepository<Transaction, String> {
+
+
+
+}
