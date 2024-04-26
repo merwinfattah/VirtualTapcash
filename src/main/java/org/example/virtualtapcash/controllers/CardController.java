020c8adb532bb259615ca9c48051c7c29d4254ee
merwinfattah <merwinfattah@gmail.com>
2024-04-26 08:33:48 +0700

    Removed Unnecessary Dependency

--- src/main/java/org/example/virtualtapcash/controllers/CardController.java
+++ src/main/java/org/example/virtualtapcash/controllers/CardController.java
@@ -1,12 +1,6 @@
 package org.example.virtualtapcash.controllers;
 
 import org.example.virtualtapcash.entities.AddCardRequest;
-import org.example.virtualtapcash.models.ExternalSystemCard;
-import org.example.virtualtapcash.models.MBankingAccount;
-import org.example.virtualtapcash.models.TapcashCard;
-import org.example.virtualtapcash.repository.TapcashCardJpaRepository;
-import org.example.virtualtapcash.repository.UserJpaRepository;
-import org.example.virtualtapcash.services.ExternalSystemCardService;
 import org.example.virtualtapcash.services.TapcashCardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,11 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Comparator;
-import java.util.Date;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
+
 
 @Controller
 @RequestMapping("/api/v1/card")
