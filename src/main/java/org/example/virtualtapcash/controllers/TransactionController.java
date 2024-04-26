bb2dd7d885335e887011c5dfd28c63bc8cc7c69d
ZarusILie <lazarusliekarsono@gmail.com>
2024-04-26 09:10:34 +0700

    update TransactionController.java and TransactionService.java

--- src/main/java/org/example/virtualtapcash/controllers/TransactionController.java
+++ src/main/java/org/example/virtualtapcash/controllers/TransactionController.java
@@ -2,6 +2,7 @@ package org.example.virtualtapcash.controllers;
 
 import org.example.virtualtapcash.entities.PaymentRequest;
 import org.example.virtualtapcash.entities.TransactionRequest;
+import org.example.virtualtapcash.entities.TransactionResult;
 import org.example.virtualtapcash.exceptions.ErrorTransaction;
 import org.example.virtualtapcash.models.MBankingAccount;
 import org.example.virtualtapcash.models.TapcashCard;
@@ -29,130 +30,41 @@ public class TransactionController {
     @Autowired
     private  TransactionService transactionService;
 
-    @Autowired
-    private TransactionJpaRepository transactionJpaRepository;
-
-    @Autowired
-    private TapcashCardJpaRepository tapcashCardJpaRepository;
-
-    @Autowired
-    private UserJpaRepository userJpaRepository;
-
-
-
     @GetMapping("/get-transaction-data/{rfid}")
     public ResponseEntity<?> getTransactionData(@PathVariable String rfid) {
         try {
-            List<Transaction> transaction = transactionJpaRepository.findTransactionsByRfid(rfid);
-            if(transaction.isEmpty()) {
-                return ResponseEntity.ok(transaction);
-            } else {
-                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No Transactions Data Found");
-            }
+            List<Transaction> transactions = transactionService.getTransactionsByRfid(rfid);
+            return ResponseEntity.ok(transactions);
+        } catch (CardNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
         } catch (Exception e) {
             String errorMessage = "An error occurred while processing the request.";
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorMessage);
         }
-
     }
 
     @PostMapping("/payment")
     public ResponseEntity<?> createPayment(@RequestBody PaymentRequest paymentRequest) {
         try {
-            TapcashCard card = tapcashCardJpaRepository.findTapcashCardsByRfid(paymentRequest.getRfid())
-                    .orElseThrow(() -> new CardNotFoundException("Card not found with RFID: " + paymentRequest.getRfid()));
-
-            // Check if the card has sufficient balance
-            // Assuming paymentRequest.getNominal() returns a BigDecimal
-            BigDecimal minRequiredBalance = paymentRequest.getNominal().add(new BigDecimal("4000"));
-            if (card.getTapCashBalance().compareTo(minRequiredBalance) <= 0) {
-                throw new InsufficientFundsException("Insufficient funds available. A minimum balance of 4000 over the transaction amount is required.");
-            }
-
-
-            // Deduct the transaction amount from the card's balance
-            card.setTapCashBalance(card.getTapCashBalance().subtract(paymentRequest.getNominal()));
-            tapcashCardJpaRepository.save(card);
-
-            // Record the transaction
-            Transaction transaction = new Transaction();
-
-            transaction.setCard(card);
-            transaction.setNominal(paymentRequest.getNominal());
-            transaction.setCreatedAt(new Date());
-            transaction.setUser(card.getUser());
-
-            transactionJpaRepository.save(transaction);
-
-            // Return a success result
-            return ResponseEntity.status(HttpStatus.OK).body("Payment Successfull");
-
-        } catch (InsufficientFundsException e) {
-            return ResponseEntity.badRequest().body("Insufficient funds");
-        } catch (CardNotFoundException e) {
-            return ResponseEntity.notFound().build();
+            TransactionResult result = transactionService.processPayment(paymentRequest.getRfid(), paymentRequest.getNominal());
+            return ResponseEntity.ok(result.getMessage());
+        } catch (InsufficientFundsException | CardNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         } catch (Exception e) {
-            String errorMessage = "An error occurred while processing the request.";
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorMessage);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred while processing the request.");
         }
     }
 
     @PostMapping("/top-up-n-withdraw")
     public ResponseEntity<?> createTransaction(@RequestBody TransactionRequest transactionRequest) {
         try {
-            TapcashCard card = tapcashCardJpaRepository.findTapcashCardsByRfid(transactionRequest.getRfid())
-                    .orElseThrow(() -> new CardNotFoundException("Card not found with RFID: " + transactionRequest.getRfid()));
-
-            MBankingAccount mBankingAccount = userJpaRepository.getUserByVirtualTapcashId(transactionRequest.getVirtual_tapcash_id())
-                    .orElseThrow(() -> new CardNotFoundException("Card not found with Virtual Tapcash Id: " + transactionRequest.getVirtual_tapcash_id()));
-
-            if ("TOPUP".equals(transactionRequest.getType())) {
-                // Check if the card's balance will exceed the limit after adding the transaction amount
-                BigDecimal totalBalanceAfterTopUp = card.getTapCashBalance().add(transactionRequest.getNominal());
-                if (totalBalanceAfterTopUp.compareTo(BigDecimal.valueOf(2000000)) > 0) {
-                    throw new ErrorTransaction("Top-up amount exceeds maximum limit.");
-                }
-
-                // Deduct the transaction amount from the user's bank account balance
-                mBankingAccount.setBankAccountBalance(mBankingAccount.getBankAccountBalance().subtract(transactionRequest.getNominal()));
-                userJpaRepository.save(mBankingAccount);
-
-                // Add the transaction amount to the card's balance
-                card.setTapCashBalance(card.getTapCashBalance().add(transactionRequest.getNominal()));
-                tapcashCardJpaRepository.save(card);
-            } else if ("WITHDRAW".equals(transactionRequest.getType())) {
-                // Check if the card's balance will exceed the limit after subtracting the transaction amount
-                BigDecimal totalWithdraw = card.getTapCashBalance().subtract(transactionRequest.getNominal());
-                if (totalWithdraw.compareTo(BigDecimal.ZERO) < 0) {
-                    throw new ErrorTransaction("Withdrawal amount exceeds available balance.");
-                }
-
-                // Subtract the transaction amount from the card's balance
-                card.setTapCashBalance(card.getTapCashBalance().subtract(transactionRequest.getNominal()));
-                tapcashCardJpaRepository.save(card);
-
-                // Add the transaction amount to the user's bank account balance
-                mBankingAccount.setBankAccountBalance(mBankingAccount.getBankAccountBalance().add(transactionRequest.getNominal()));
-                userJpaRepository.save(mBankingAccount);
-            }
-
-            // Record the transaction
-            Transaction transaction = new Transaction();
-            transaction.setCard(card);
-            transaction.setNominal(transactionRequest.getNominal());
-            transaction.setCreatedAt(new Date()); // Ensure you set all required fields
-            transactionJpaRepository.save(transaction);
-
-            // Return a success result
-            return ResponseEntity.status(HttpStatus.OK).body("Transaction Successfull");
-
-        } catch (ErrorTransaction e) {
-            return ResponseEntity.badRequest().body("ERROR TRANSACTION");
-        } catch (CardNotFoundException e) {
-            return ResponseEntity.notFound().build();
+            TransactionResult result = transactionService.handleTopUpWithdrawal(transactionRequest.getRfid(), transactionRequest.getNominal(), transactionRequest.getType(), transactionRequest.getVirtual_tapcash_id());
+            return ResponseEntity.ok(result.getMessage());
+        } catch (ErrorTransaction | CardNotFoundException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
         } catch (Exception e) {
-            String errorMessage = "An error occurred while processing the request.";
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorMessage);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred while processing the request.");
         }
     }
+
 }
--- src/main/java/org/example/virtualtapcash/services/TransactionService.java
+++ src/main/java/org/example/virtualtapcash/services/TransactionService.java
@@ -4,10 +4,13 @@ package org.example.virtualtapcash.services;
 
 
 import org.example.virtualtapcash.entities.TransactionResult;
+import org.example.virtualtapcash.exceptions.ErrorTransaction;
 import org.example.virtualtapcash.models.TapcashCard;
 import org.example.virtualtapcash.models.Transaction;
+import org.example.virtualtapcash.models.MBankingAccount;
 import org.example.virtualtapcash.repository.TapcashCardJpaRepository;
 import org.example.virtualtapcash.repository.TransactionJpaRepository;
+import org.example.virtualtapcash.repository.UserJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,6 +26,12 @@ import java.util.Optional;
 public class TransactionService {
     @Autowired
     private TransactionJpaRepository transactionJpaRepository;
+
+    @Autowired
+    private TapcashCardJpaRepository tapcashCardJpaRepository;
+
+    @Autowired
+    private UserJpaRepository userJpaRepository;
 
 
     public Transaction createTransaction(Transaction transaction) { return transactionJpaRepository.save(transaction);
@@ -40,6 +49,74 @@ public class TransactionService {
 
     public Transaction updateTransaction (Transaction transaction) { return transactionJpaRepository.save(transaction);
     }
+
+    public List<Transaction> getTransactionsByRfid(String rfid) throws CardNotFoundException {
+        List<Transaction> transactions = transactionJpaRepository.findTransactionsByRfid(rfid);
+        if (transactions.isEmpty()) {
+            throw new CardNotFoundException("No Transactions Data Found for RFID: " + rfid);
+        }
+        return transactions;
+    }
+
+    @Transactional
+    public TransactionResult processPayment(String rfid, BigDecimal nominal) throws CardNotFoundException, InsufficientFundsException {
+        TapcashCard card = tapcashCardJpaRepository.findTapcashCardsByRfid(rfid)
+                .orElseThrow(() -> new CardNotFoundException("Card not found with RFID: " + rfid));
+
+        BigDecimal minRequiredBalance = nominal.add(new BigDecimal("4000"));
+        if (card.getTapCashBalance().compareTo(minRequiredBalance) <= 0) {
+            throw new InsufficientFundsException("Insufficient funds available. A minimum balance of 4000 over the transaction amount is required.");
+        }
+
+        card.setTapCashBalance(card.getTapCashBalance().subtract(nominal));
+        tapcashCardJpaRepository.save(card);
+
+        Transaction transaction = new Transaction();
+        transaction.setCard(card);
+        transaction.setNominal(nominal);
+        transaction.setCreatedAt(new Date());
+        transaction.setUser(card.getUser());
+        transactionJpaRepository.save(transaction);
+
+        return new TransactionResult(true, "Payment successful");
+    }
+
+    @Transactional
+    public TransactionResult handleTopUpWithdrawal(String rfid, BigDecimal nominal, String type, String virtualTapcashId) throws CardNotFoundException, ErrorTransaction {
+        TapcashCard card = tapcashCardJpaRepository.findTapcashCardsByRfid(rfid)
+                .orElseThrow(() -> new CardNotFoundException("Card not found with RFID: " + rfid));
+
+        MBankingAccount mBankingAccount = userJpaRepository.getUserByVirtualTapcashId(String.valueOf(virtualTapcashId))
+                .orElseThrow(() -> new CardNotFoundException("Card not found with Virtual Tapcash Id: " + virtualTapcashId));
+
+        if ("TOPUP".equals(type)) {
+            BigDecimal totalBalanceAfterTopUp = card.getTapCashBalance().add(nominal);
+            if (totalBalanceAfterTopUp.compareTo(BigDecimal.valueOf(2000000)) > 0) {
+                throw new ErrorTransaction("Top-up amount exceeds maximum limit.");
+            }
+            mBankingAccount.setBankAccountBalance(mBankingAccount.getBankAccountBalance().subtract(nominal));
+            card.setTapCashBalance(totalBalanceAfterTopUp);
+        } else if ("WITHDRAW".equals(type)) {
+            BigDecimal totalWithdraw = card.getTapCashBalance().subtract(nominal);
+            if (totalWithdraw.compareTo(BigDecimal.ZERO) < 0) {
+                throw new ErrorTransaction("Withdrawal amount exceeds available balance.");
+            }
+            mBankingAccount.setBankAccountBalance(mBankingAccount.getBankAccountBalance().add(nominal));
+            card.setTapCashBalance(totalWithdraw);
+        }
+        userJpaRepository.save(mBankingAccount);
+        tapcashCardJpaRepository.save(card);
+
+        Transaction transaction = new Transaction();
+        transaction.setCard(card);
+        transaction.setNominal(nominal);
+        transaction.setCreatedAt(new Date());
+        transaction.setUser(card.getUser());
+        transactionJpaRepository.save(transaction);
+
+        return new TransactionResult(true, type + " successful");
+    }
+
 
 
 }
