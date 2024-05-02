7ba85accf78cb0360951d5bd3e1ba1f01a91d475
Gilang Deva <66029596+gilangdeva@users.noreply.github.com>
2024-05-02 11:50:00 +0700

    Add Sort By Latest Transaction

--- src/main/java/org/example/virtualtapcash/repository/TransactionJpaRepository.java
+++ src/main/java/org/example/virtualtapcash/repository/TransactionJpaRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface TransactionJpaRepository extends JpaRepository<Transaction, Long> {
-    @Query(value = "SELECT * FROM tb_transaction WHERE card_id = ?1 ORDER BY created_at ASC", nativeQuery = true)
+    @Query(value = "SELECT * FROM tb_transaction WHERE card_id = ?1  ORDER BY STR_TO_DATE(created_at, '%Y-%m-%d %H:%i:%s') DESC", nativeQuery = true)
     List<Transaction> findTransactionsByCardId(String cardId);
 
 }
