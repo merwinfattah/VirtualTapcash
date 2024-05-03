35775dbbbd20a0d067ff0735843bd664669b342f
ZarusILie <lazarusliekarsono@gmail.com>
2024-05-03 08:01:48 +0700

    Add Security for QRPayment

---
+++ src/main/java/org/example/virtualtapcash/service/QrService.java
@@ -0,0 +1,82 @@
+package org.example.virtualtapcash.service;
+
+import jakarta.transaction.Transactional;
+import org.example.virtualtapcash.model.QR;
+import org.example.virtualtapcash.repository.QrJpaRepository;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
+@Service
+public class QrService {
+    private final QrJpaRepository qrJpaRepository;
+
+    public QrService(QrJpaRepository qrGeneratedRepository) {
+        this.qrJpaRepository = qrGeneratedRepository;
+    }
+
+    public boolean checkIsActiveByCardId(String cardId) {
+        QR qr = qrJpaRepository.findActiveQRCodeByCardId(cardId);
+        return qr != null && qr.getIsActive() && qr.getActivationTime().plusMinutes(1).isAfter(LocalDateTime.now());
+    }
+
+    @Transactional
+    public boolean activateQrCodeByCardId(String cardId) {
+        List<QR> inactiveOrNullQRCodes = qrJpaRepository.findInactiveOrNullQRCodesByCardId(cardId);
+        if (!inactiveOrNullQRCodes.isEmpty()) {
+            QR qr = inactiveOrNullQRCodes.get(0); // Assuming you activate the first one found
+            qr.setIsActive(true);
+            qr.setActivationTime(LocalDateTime.now());
+            qrJpaRepository.save(qr);
+            return true;
+        }
+        return false;
+    }
+
+    @Scheduled(fixedRate = 60000) // Every 30 seconds
+    public void deactivateExpiredQRCodes() {
+        LocalDateTime oneMinuteAgo = LocalDateTime.now().minusMinutes(1);
+        List<QR> expiredQRCodes = qrJpaRepository.findExpiredQRCodes(oneMinuteAgo);
+        for (QR qr : expiredQRCodes) {
+            qr.setIsActive(false);
+            qrJpaRepository.save(qr);
+        }
+    }
+
+
+    @Transactional
+    public void deactivateQrCodeByCardId(String cardId) {
+        QR qr = qrJpaRepository.findActiveQRCodeByCardId(cardId);
+        if (qr != null && qr.getIsActive()) {
+            qr.setIsActive(false);
+            qrJpaRepository.save(qr);
+        }
+    }
+
+    public boolean checkIsActive(long qrId) {
+        QR qr = qrJpaRepository.findById(qrId).orElseThrow(() -> new RuntimeException("QR Code not found"));
+        return qr.getIsActive() && qr.getActivationTime().plusMinutes(1).isAfter(LocalDateTime.now());
+    }
+
+    @Transactional
+    public boolean deactivateQrCodeByCardIdImmediately(String cardId) {
+        QR qr = qrJpaRepository.findActiveQRCodeByCardId(cardId);
+        if (qr != null && qr.getIsActive()) {
+            qr.setIsActive(false);
+            qrJpaRepository.save(qr);
+            // Set up a timer to reactivate it after 1 minute
+            new Timer().schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    deactivateQrCodeByCardId(cardId);
+                }
+            }, 60000); // 60 seconds
+            return true;
+        }
+        return false;
+    }
+}
