c6eaed857ac9a2b5ef9945c542f39d3e05d3e8f2
Gilang Deva <gilangdevaa@gmail.com>
2024-05-07 14:07:48 +0700

    Update AccountJpaRepository.java

--- src/main/java/org/example/virtualtapcash/repository/AccountJpaRepository.java
+++ src/main/java/org/example/virtualtapcash/repository/AccountJpaRepository.java
@@ -1,16 +1,19 @@
 package org.example.virtualtapcash.repository;
 
 import org.example.virtualtapcash.model.MBankingAccount;
+import org.example.virtualtapcash.model.TapcashCard;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 
 public interface AccountJpaRepository extends JpaRepository<MBankingAccount, Long> {
-    @Query("SELECT t FROM MBankingAccount t WHERE t.virtualTapCashId = :virtual_tapcash_id")
-    Optional<MBankingAccount> getUserByVirtualTapcashId(@Param("virtual_tapcash_id") String virtual_tapcash_id);
+    @Query(value = "SELECT * FROM tb_tapcash_card WHERE virtual_tapcash_id =?1", nativeQuery = true)
+    Optional<MBankingAccount> getUserByVirtualTapcashId(String virtualTapcashId);
+
     Optional<MBankingAccount> findByUsername(String name);
 
     Optional<MBankingAccount> findMBankingAccountByVirtualTapCashId(String virtualTapcashId);
