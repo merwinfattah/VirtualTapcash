d0af1c0fc8ac9c3f450e5034d42b0c6d9f725119
merwinfattah <merwinfattah@gmail.com>
2024-05-08 10:57:14 +0700

    Configure CORS

--- src/main/java/org/example/virtualtapcash/VirtualTapcashApplication.java
+++ src/main/java/org/example/virtualtapcash/VirtualTapcashApplication.java
@@ -15,8 +15,7 @@ public class VirtualTapcashApplication {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/v1/**").allowedOrigins("https://virtual-tapcash.netlify.app");
-//                "http://localhost:4200",
+                registry.addMapping("/api/v1/**").allowedOrigins("http://localhost:4200", "https://virtual-tapcash.netlify.app");
             }
         };
     }
