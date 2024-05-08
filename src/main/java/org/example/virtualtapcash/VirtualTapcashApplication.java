e0892695ba0a05320a83b6a197ebd197733aeb9b
merwinfattah <merwinfattah@gmail.com>
2024-05-08 09:29:23 +0700

    Configure Swagger

--- src/main/java/org/example/virtualtapcash/VirtualTapcashApplication.java
+++ src/main/java/org/example/virtualtapcash/VirtualTapcashApplication.java
@@ -6,8 +6,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class VirtualTapcashApplication {
 
     @Bean
@@ -15,9 +21,18 @@ public class VirtualTapcashApplication {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/v1/**").allowedOrigins("https://virtual-tapcash.netlify.app");
+                registry.addMapping("/api/v1/**").allowedOrigins("https://localhost:4200");
             }
         };
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
     }
 
     public static void main(String[] args) {
